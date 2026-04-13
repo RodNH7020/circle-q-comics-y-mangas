f294948e5121e1f25c1a0af14ae42fe7285464bd
Rodrigo <mipapa300.com@gmail.com>
2026-04-13 18:57:08 -0300

    Primeros avances (Se agrego la imagen de fondo y creamos la plantilla para todas las paginas, hicimos el navbar )

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+@extends('layouts.app')
+
+@section('title', 'Inicio')
+
+@section('content')
+<div class="d-flex justify-content-center align-items-start vh-100">
+    <div class="text-center">
+        <h1>Bienvenido</h1>
+        <p>Explorá los mejores cómics</p>
+    </div>
+</div>
+@endsection
+    
+
+</body>
+</html>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title')</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: Arial;
+            margin: 0;
+
+            background-image: 
+                linear-gradient(rgba(120,0,0,0.8), rgba(120,0,0,0.8)),
+                 url('/images/fondo-heroes.jpg.jpg');
+            background-size: cover;
+            background-position: center;
+        }
+
+        .navbar {
+            background-color: rgba(0,0,0,0.8);
+        }
+    </style>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-dark px-3">
+        <span class="navbar-brand">Circle Q</span>
+        <a href="/home">Inicio</a>
+        <a href="/quienes-somos">Quiénes somos</a>
+        <a href="/comercializacion">Comercialización</a>
+        <a href="/consultas">Consultas</a>
+        <a href="/contacto">Contacto</a>
+    </nav>
+
+    <div class="container mt-4">
+        @yield('content')
+    </div>
+
+</body>
+</html>
