c092bfd7df457b86964e51a250d2938b6a59b200
Lourdes <lourdeselizabethmonzon@gmail.com>
2026-04-15 15:26:23 -0300

    AVANCE CON EL FOOTER

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,5 +40,60 @@
         @yield('content')
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<footer class="mt-auto pt-5 pb-4" style="background-color: rgba(120, 0, 0, 0.9); color: white;">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 mb-3">
+        <h5>About Us</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ante mollis quam tristique convallis.</p>
+      </div>
+
+      <div class="col-md-4 mb-3">
+        <h5>Contactanos </h5>
+        <ul class="list-unstyled">
+          <li>
+            <a href="https://wa.me/5493794026500" target="_blank" rel="noopener noreferrer" class="text-decoration-none text-white">
+                3794026500
+            </a>
+        </li>
+
+        <li class="mt-2">
+            <a href="mailto:loourdeeselizabethmonzon@gmail.com" class="text-decoration-none text-white">
+                circleq@gmail.com
+            </a>
+        </li>
+        </ul>
+    
+      </div>
+      <div class="col-md-4 mb-3">
+        <h5>Seguinos </h5>
+        <ul class="list-inline social-icons">
+  <li class="list-inline-item">
+    <a href="https://www.facebook.com/" target="_blank" class="text-white">
+      <i class="bi bi-facebook"></i>
+    </a>
+  </li>
+
+  <li class="list-inline-item">
+    <a href="https://www.instagram.com/boludeco_" target="_blank" class="text-white">
+      <i class="bi bi-instagram"></i>
+    </a>
+  </li>
+</ul>
+      </div>
+    </div>
+    <hr class="mb-4">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <p>&copy; Copyright 2026 ©Circleq . Todos los derechos reservados </p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
 </body>
 </html>
